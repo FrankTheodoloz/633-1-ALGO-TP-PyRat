032b068793b50cccb019e24dde5e919f6b66d74c
Frank Théodoloz <frank@16d.ch>
2020-01-04 20:19:35 +0100

    Corrected static typing with typings.py + code uniformisation
    https://docs.python.org/3/library/typing.html

--- AIs/tools/breadth_first_search.py
+++ AIs/tools/breadth_first_search.py
@@ -1,60 +1,64 @@
-"""
-Breadth First Search (parcours en largeur)
-"""
+# Breadth First Search (parcours en largeur)
+from typing import Tuple, List, Dict, Union
+
+Node = Tuple[int, int]
 
 
-def breadth_first_search(graph, source_node):
-    graph_queue = [(source_node, [source_node])]  # ajoute le noeud source dans la queue
-    visited_vertices = [source_node]  # ajoute le noeud comme visité
+def breadth_first_search(graph: List[List[int]], source_node: int) -> None:
+    """ Function BFS that prints how the algorithm is visiting nodes """
+    graph_queue: List[Tuple[int, List[int]]] = [(source_node, [source_node])]  # add source_node to queue
+    visited_vertices: List[int] = [source_node]  # add source_node as visited
 
-    while graph_queue:  # si il reste quelque chose dans la queue
-        (currentNode, path) = graph_queue[0]  # alloue à currentNode et path le valeurs de la queue
-        graph_queue = graph_queue[1:]  # enlève l'entrée de la queue
-        print(currentNode, "path = ", repr(path))  # écrit les valeurs
+    while graph_queue:  # while there is a neighbour to visit in the queue
+        (current_node, path) = graph_queue[0]  # get the current_node and his path
+        graph_queue = graph_queue[1:]  # remove the current_node from the queue
+        print(current_node, "path = ", repr(path))
 
-        for neighbor in graph[currentNode]:  # parcours les voisins du currentNode
-            if neighbor not in visited_vertices:  # vérifie que le voisin n'a pas déjà été parcouru
+        for neighbour in graph[current_node]:  # for each neighbour
+            if neighbour not in visited_vertices:  # not visited
+                graph_queue.append((neighbour, path + [neighbour]))  # add neighbour with his path to the queue
+                visited_vertices.append(neighbour)  # add the neighbour as visited
+
+
+def breadth_first_search_routing(graph: List[List], source_node: int) -> List[int]:
+    """ Function BFS that return a list of routes from source_node to all other nodes """
+    graph_queue: List[Tuple[int, List[int]]] = [(source_node, [source_node])]  # add source_node to queue
+    visited_vertices: List[int] = [source_node]  # add source_node as visited
+    routes: List[Union[int, None]] = [None for i in range(len(graph))]  # init routing table of the graph size
+
+    while graph_queue:  # while there is a neighbour to visit in the queue
+        (current_node, path) = graph_queue.pop(0)  # get the current_node and his path
+        # print(current_node, "path = ", repr(path))
+
+        for neighbour in graph[current_node]:  # for each neighbour
+            if neighbour not in visited_vertices:  # not visited
+                graph_queue.append((neighbour, path + [neighbour]))  # add neighbour with his path to the queue
+                visited_vertices.append(neighbour)  # add the neighbour as visited
+                routes[neighbour] = current_node  # adds current_node as path for the neighbour
+
+    return routes
+
+
+# For Pyrat maze
+def breadth_first_search_routing_maze(graph: Dict[Node, List[Node]], source_node: Node) -> Dict[Node, Node]:
+    """ Function BFS that return a list of routes from source_node to all other nodes """
+    graph_queue: List[Tuple[Node, List[Node]]] = [(source_node, [source_node])]  # add source_node to queue
+    visited_vertices: List[Node] = [source_node]  # add source_node as visited
+    routes: Dict[Node, Node] = {}  # crée un dictionnaire
+
+    while graph_queue:  # while there is a neighbour to visit in the queue
+        current_node: Node
+        path: List[Node]
+        (current_node, path) = graph_queue.pop(0)  # get the current_node and his path
+        # print(current_node, "path = ", repr(path))
+
+        for neighbour in graph.get(current_node):
+            if neighbour not in visited_vertices:  # for each neighbour not visited
                 graph_queue.append(
-                    (neighbor, path + [neighbor]))  # ajoute le voisin avec son chemin (parcouru + lui-même)
-                visited_vertices.append(neighbor)  # ajoute le voisin comme visité
+                    (neighbour, path + [neighbour]))  # ajoute le voisin avec son chemin (parcouru + lui-même)
+                visited_vertices.append(neighbour)  # add the current_node as visited
+                routes[neighbour] = current_node  # ajoute le noeud courant comme chemin pour aller au voisin
 
-
-def breadth_first_search_routing(graph, source_node):
-    graph_queue = [(source_node, [source_node])]  # ajoute le noeud source dans la queue
-    visited_vertices = [source_node]  # ajoute le noeud comme visité
-    routes = [None for i in range(len(graph))]  # crée une table de routage de la taille du graph (NEW)
-
-    while graph_queue:  # si il reste quelque chose dans la queue
-        (current_node, path) = graph_queue.pop(0)  # alloue à current_node et path le valeurs de la queue
-        print(current_node, "path = ", repr(path))  # écrit les valeurs
-
-        for neighbor in graph[current_node]:  # parcours les voisins du current_node
-            if neighbor not in visited_vertices:  # vérifie que le voisin n'a pas déjà été parcouru
-                graph_queue.append(
-                    (neighbor, path + [neighbor]))  # ajoute le voisin avec son chemin (parcouru + lui-même)
-                visited_vertices.append(neighbor)  # ajoute le voisin comme visité
-                routes[neighbor] = current_node  # ajoute le noeud courant comme chemin pour aller au voisin
-
-    print("routes :", repr(routes))  # affiche les routes
-
-
-def breadth_first_search_routing_maze(graph, source_node):
-    graph_queue = [(source_node, [source_node])]  # ajoute le noeud source dans la queue
-    visited_vertices = [source_node]  # ajoute le noeud comme visité
-    routes = {}  # crée un dictionnaire
-
-    while graph_queue:  # si il reste quelque chose dans la queue
-        (current_node, path) = graph_queue.pop(0)  # alloue à current_node et path le valeurs de la queue
-        # print(current_node, "path = ", repr(path))  # écrit les valeurs
-
-        for neighbor in graph.get(current_node):  # parcours les voisins du current_node
-            if neighbor not in visited_vertices:  # vérifie que le voisin n'a pas déjà été parcouru
-                graph_queue.append(
-                    (neighbor, path + [neighbor]))  # ajoute le voisin avec son chemin (parcouru + lui-même)
-                visited_vertices.append(neighbor)  # ajoute le voisin comme visité
-                routes[neighbor] = current_node  # ajoute le noeud courant comme chemin pour aller au voisin
-
-    print("routes :", repr(routes))  # affiche les routes
     return routes
 
 
@@ -62,15 +66,22 @@ def breadth_first_search_routing_maze(graph, source_node):
 Test breadth_first_search
 '''
 # graph = [[1, 2, 5], [0, 2, 5], [0, 1], [4, 5], [3, 5], [0, 1, 3, 4]]
-# breadth_first_search(graph, 0)
+# print(repr(breadth_first_search(graph, 0)))
+# 0 path =  [0]
+# 1 path =  [0, 1]
+# 2 path =  [0, 2]
+# 5 path =  [0, 5]
+# 3 path =  [0, 5, 3]
+# 4 path =  [0, 5, 4]
+# None
 
 '''
 Test breadth_first_search_routing
 '''
 # graph = [[1, 2, 5], [0, 2, 5], [0, 1], [4, 5], [3, 5], [0, 1, 3, 4]]
-# breadth_first_search_routing(graph, 0)
-
-# routes :  [None, 0, 0, 5, 5, 0] : pour aller à 4 il faut passer par 5 pour aller à 5 il faut passer par 0 donc 0, 5, 4
+# print("Routes:", repr(breadth_first_search_routing(graph, 0)))
+#
+# Routes:  [None, 0, 0, 5, 5, 0] : pour aller à 4 il faut passer par 5 pour aller à 5 il faut passer par 0 donc 0, 5, 4
 
 '''
 Test breadth_first_search_routing_maze
@@ -93,8 +104,8 @@ Test breadth_first_search_routing_maze
 #         (6, 0): {(5, 0): 1, (6, 1): 1}, (6, 1): {(6, 2): 1, (6, 0): 1}, (6, 2): {(6, 3): 1, (6, 1): 1},
 #         (6, 3): {(5, 3): 1, (6, 2): 1}, (6, 4): {(6, 5): 1}, (6, 5): {(6, 4): 1, (5, 5): 1}, (6, 6): {(5, 6): 1}}
 # location = (0, 0)
-# breadth_first_search_routing_maze(maze, location)
-
+# print("Routes:", repr(breadth_first_search_routing_maze(maze, location)))
+#
 # routes : {(1, 0): (0, 0),  # 1
 #           (2, 0): (1, 0),  # 2
 #           (1, 1): (1, 0),
